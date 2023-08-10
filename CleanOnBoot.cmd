7656eb241ba82fc6c2ceef545f7eb35ab09b60f5
John Roniel <60148238+JRoniel@users.noreply.github.com>
2023-08-10 01:25:06 -0300

    Update and rename aw.cmd to CleanOnBoot.cmd
    Atualização e renomeação do código

--- CleanOnBoot.cmd
+++ CleanOnBoot.cmd
@@ -1,11 +1,8 @@
 # FUNCIONALIDADE:
 # LIMPA MEMORIA TEMPORARIA E ENCERRA TAREFAS DESNCESSARIAS
-# OTIMO PARA SER EXECUTADO LOGO APÓS A INICIALIZAÇÃO DO COMPUTADOR.
 
 @echo off
 color a
-title AW - Delivery 0.3v
 del /q /f /s %windir%\Temp
 %windir%\system32\rundll32.exe advapi32.dll,ProcessIdleTasks
-start /min https://web.whatsapp.com/
 timeout 7
